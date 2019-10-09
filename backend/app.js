17d689445c4e8ef49380e0ac7bcdbfeb672cd5af
icarus8050 <icarus8050@naver.com>
2019-10-10 00:07:56 +0900

    회원 가입 기능 구현

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const auth = require('./routes/auth');
 const sequelize = require('./models').sequelize;
 
 const app = express();
@@ -34,6 +35,7 @@ app.use(session({
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -47,8 +49,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).json(err);
 });
 
 module.exports = app;
