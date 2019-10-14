e441632d5c7efe0893c53c5f5c01d061db363557
icarus8050 <icarus8050@naver.com>
2019-10-14 23:51:18 +0900

    passport 따라쳐보기..

---
+++ backend/passport/index.js
@@ -0,0 +1,16 @@
+const local = require('./localStrategy');
+const {User} = require('../models');
+
+module.exports = (passport) => {
+    passport.serializeUser((user, done) => {
+        done(null, user.userId);
+    });
+
+    passport.deserializeUser((userId, done) => {
+        User.find({where: {userId}})
+            .then(user => done(null, user))
+            .catch(err => done(err));
+    });
+
+    local(passport);
+};
