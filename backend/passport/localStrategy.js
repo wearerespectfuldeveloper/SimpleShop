a1abea8800e227db3fd997679b4ef840f484da65
icarus8050 <icarus8050@naver.com>
2019-10-15 23:54:36 +0900

    라인 정리..

--- backend/passport/localStrategy.js
+++ backend/passport/localStrategy.js
@@ -4,28 +4,32 @@ const bcrypt = require('bcrypt-nodejs');
 const {User} = require('../models');
 
 module.exports = (passport) => {
-    passport.use(new LocalStrategy({
-            usernameField: 'userId',
-            passwordField: 'password',
-            passReqToCallback: true
-        }, async (req, userId, password, done) => {
-            try {
-                const findUser = await User.find({where: {userId}});
-                if (findUser) {
-                    const result = bcrypt.compareSync(password, findUser.password);
+    passport.use(
+        new LocalStrategy(
+            {
+                usernameField: 'userId',
+                passwordField: 'password',
+                passReqToCallback: true
+            },
+            async (req, userId, password, done) => {
+                try {
+                    const findUser = await User.find({where: {userId}});
+                    if (findUser) {
+                        const result = bcrypt.compareSync(password, findUser.password);
 
-                    if (result) {
-                        done(null, findUser);
+                        if (result) {
+                            done(null, findUser);
+                        } else {
+                            done(null, false, {message: '비밀번호가 일치하지 않습니다.'});
+                        }
                     } else {
-                        done(null, false, {message: '비밀번호가 일치하지 않습니다.'});
+                        done(null, false, {message: '가입되지 않은 회원입니다.'});
                     }
-                } else {
-                    done(null, false, {message: '가입되지 않은 회원입니다.'});
+                } catch (error) {
+                    console.log(error);
+                    done(error);
                 }
-            } catch (error) {
-                console.log(error);
-                done(error);
             }
-        })
+        )
     );
 };
