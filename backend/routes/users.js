f74f8ef4c9b4ad21cd212439de46d215448f3b0a
icarus8050 <icarus8050@naver.com>
2019-10-27 01:38:35 +0900

    userID 중복 체크

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,21 +3,31 @@ const router = express.Router();
 const User = require('../models').User;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/:userId/check', async (req, res, next) => {
+    let user = await User.findOne({
+        where: {
+            userId: req.params.userId
+        }
+    });
+
+    if (user) {
+        res.status(500).json("userId is not available");
+    } else {
+        res.status(200).json("ok");
+    }
 });
 
 router.post('/', function (req, res, next) {
-  User.create({
-    userId: 'icarus',
-    password: 'aaa',
-    name: 'chul'
-  }).then((user) => {
-    res.json(user);
-  }).catch((err) => {
-    console.log(err);
-    next(err);
-  })
+    User.create({
+        userId: 'icarus',
+        password: 'aaa',
+        name: 'chul'
+    }).then((user) => {
+        res.json(user);
+    }).catch((err) => {
+        console.log(err);
+        next(err);
+    })
 });
 
 module.exports = router;
